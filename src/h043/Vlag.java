0e6b49cd8f2f2edc5be83bfe00fc466569ede491
Dennis <dkoppen@roc-dev.com>
2016-09-08 15:57:13 +0200

    lukt het

---
+++ src/h043/Vlag.java
@@ -0,0 +1,15 @@
+package h043;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Vlag extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+}
